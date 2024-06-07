b084ecfba30386875c7ed594a9285094a353ea2e
Launacloud <109642716+Launacloud@users.noreply.github.com>
2024-06-07 02:00:09 -0300

    Update feed_generator.py

--- feed_generator.py
+++ feed_generator.py
@@ -1,3 +1,5 @@
+# Import the feeds list from feed.py
+from feed import feeds
 import os
 from urllib.parse import urljoin
 from datetime import datetime
@@ -6,28 +8,7 @@ from feedgen.feed import FeedGenerator
 from bs4 import BeautifulSoup
 from pytz import timezone
 import json
-
-# Define the list of feeds
-feeds = [
-    {
-        "title": "Palavra do Dia RSS",
-        "subtitle": "Daily words from Dicio",
-        "url": "https://www.dicio.com.br/palavra-do-dia/",
-        "author_name": "Dicio",
-        "author_email": "contact@dicio.com.br",
-        "copyright": "Dicio",
-        "language": "pt",
-        "item_title_css": ".word-of-day .title",
-        "item_url_css": ".word-of-day--subtitle a",
-        "item_author_css": None,
-        "item_description_css": ".word-of-day--text-wrap .word-of-day--description, .word-of-day--extra",
-        "item_date_css": ".word-of-day .title",
-        "item_date_format": "%d/%m/%Y",
-        "item_timezone": "America/Sao_Paulo",
-        "output_path": "feeds/palavra_do_dia",
-        "formats": ["xml", "json"]
-    }
-]
+import xml.etree.ElementTree as ET
 
 # Function to generate feed
 def generate_feed(feed_config):
@@ -37,8 +18,10 @@ def generate_feed(feed_config):
     titles = soup.select(feed_config["item_title_css"])
     urls = soup.select(feed_config["item_url_css"])
     descriptions = soup.select(feed_config["item_description_css"]) if feed_config["item_description_css"] else []
-    authors = soup.select(feed_config["item_author_css"]) if feed_config["item_author_css"] else []
-    dates = soup.select(feed_config["item_date_css"]) if feed_config["item_date_css"] else []
+    extras = soup.select(feed_config["item_extra_css"]) if "item_extra_css" in feed_config else []  # Select first extra field
+    extras2 = soup.select(feed_config["item_extra_css2"]) if "item_extra_css2" in feed_config else []  # Select second extra field
+    authors = soup.select(feed_config["item_author_css"]) if "item_author_css" in feed_config else []
+    dates = soup.select(feed_config["item_date_css"]) if "item_date_css" in feed_config else []
 
     fg = FeedGenerator()
     fg.id(feed_config["url"])
@@ -48,27 +31,34 @@ def generate_feed(feed_config):
     fg.language(feed_config["language"])
     fg.author({'name': feed_config["author_name"], 'email': feed_config["author_email"]})
 
-    min_len = min(len(titles), len(urls), len(descriptions) or len(titles), len(authors) or len(titles), len(dates) or len(titles))
+    min_len = min(len(titles), len(urls) or len(titles), len(descriptions) or len(titles), len(authors) or len(titles), len(dates) or len(titles), len(extras) or len(titles), len(extras2) or len(titles))
 
     for i in range(min_len):
         fe = fg.add_entry()
-        fe.title(titles[i].text.strip())
-
+        fe.title(titles[i].text)
         item_url = urljoin(feed_config["url"], urls[i].get('href')) if urls else feed_config["url"]
         fe.id(item_url)
         fe.link(href=item_url, rel='alternate')
-        print("URL from entry:", item_url)  # Print URL from entry
 
-        if descriptions:
-            description_text = descriptions[i].text.strip() if i < len(descriptions) else "No description found"
-            fe.description(description_text)
+        description_text = descriptions[i].text if i < len(descriptions) else "No description found"
+
+        # Include extra information from item_extra_css and item_extra_css2
+        if extras:
+            extra_text = extras[i].text if i < len(extras) else "No extra information found"
+            description_text += f"\n\nExtra 1: {extra_text}"
+
+        if extras2:
+            extra2_text = extras2[i].text if i < len(extras2) else "No second extra information found"
+            description_text += f"\n\nExtra 2: {extra2_text}"
+
+        fe.description(description_text)
 
         if authors:
-            author_text = authors[i].text.strip() if i < len(authors) else "No author found"
+            author_text = authors[i].text if i < len(authors) else "No author found"
             fe.author(name=author_text)
 
         if dates:
-            date_text = dates[i].text.strip() if i < len(dates) else "No date found"
+            date_text = dates[i].text if i < len(dates) else "No date found"
             date_format = feed_config["item_date_format"]
             try:
                 date_obj = datetime.strptime(date_text.split()[-1][1:-1], date_format)
@@ -85,12 +75,21 @@ def generate_feed(feed_config):
     fg.atom_file(atom_file_path)
 
     # Generate JSON feed by converting Atom feed
-    atom_str = fg.atom_str()
+    atom_str = fg.atom_str(pretty=True)
     if atom_str:
         atom_str_decoded = atom_str.decode("utf-8")  # Decode bytes to string
+        feed_items = []
+
+        root = ET.fromstring(atom_str_decoded)
+        for entry in root.findall('{http://www.w3.org/2005/Atom}entry'):
+            item = {}
+            item['Title'] = entry.find('{http://www.w3.org/2005/Atom}title').text
+            item['ID'] = entry.find('{http://www.w3.org/2005/Atom}id').text
+            item['Content'] = entry.find('{http://www.w3.org/2005/Atom}content').text.strip()
+            feed_items.append(item)
+
         json_data = {
-            "atom_feed": atom_str_decoded,
-            "config": feed_config
+            "feed_items": feed_items
         }
         json_file_path = os.path.join(output_path, 'feed.json')
         with open(json_file_path, 'w') as json_file:
@@ -98,6 +97,6 @@ def generate_feed(feed_config):
     else:
         print("Atom feed is empty.")
 
-# Generate feeds for each item in the feeds list
-for feed in feeds:
-    generate_feed(feed)
+# Generate feeds for each item in the feeds list imported from feed.py
+for feed_config in feeds:
+    generate_feed(feed_config)
